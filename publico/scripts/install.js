7af0ed906d2786714e4650df90b2715e2f1ff6ce
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-12 16:52:44 -0300

    "commit v2.2"

--- publico/scripts/install.js
+++ publico/scripts/install.js
@@ -2,28 +2,30 @@
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 // CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
-
-// CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
-
-// CODELAB: Log user response to prompt.
-deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
+function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+    // CODELAB: Add code show install prompt & hide the install button.
+    deferredInstallPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    evt.srcElement.setAttribute('hidden', true);
+    // CODELAB: Log user response to prompt.
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt', choice);
+            } else {
+                console.log('User dismissed the A2HS prompt', choice);
+            }
+            deferredInstallPrompt = null;
     });
+}
 
 // CODELAB: Add event listener for appinstalled event
 window.addEventListener('appinstalled', logAppInstalled);
 
-// CODELAB: Add code to log the event
-console.log('Aplicativo instalado.', evt);
+function logAppInstalled(evt) {
+    // CODELAB: Add code to log the event
+    console.log('Aplicativo instalado.', evt);
+}
 
