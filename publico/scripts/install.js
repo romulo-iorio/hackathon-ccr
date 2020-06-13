f7694133301edc35cee42c750445d01131fe5dfa
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-12 22:38:59 -0300

    commit v2.15

--- publico/scripts/install.js
+++ publico/scripts/install.js
@@ -17,15 +17,17 @@ function installPWA(evt) {
     // Hide the install button, it can't be called twice.
     evt.target.classList.add('hidden');
     // CODELAB: Log user response to prompt.
-    deferredInstallPrompt.userChoice
-        .then((choice) => {
-            if (choice.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt', choice);
-            } else {
-                console.log('User dismissed the A2HS prompt', choice);
-            }
-            deferredInstallPrompt = null;
-        });
+    if (deferredPrompt) {
+        deferredInstallPrompt.userChoice
+            .then((choice) => {
+                if (choice.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt', choice);
+                } else {
+                    console.log('User dismissed the A2HS prompt', choice);
+                }
+                deferredInstallPrompt = null;
+            });
+    }
 }
 
 // CODELAB: Add event listener for appinstalled event
