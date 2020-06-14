35f894cc08176021d72e4b7ad9f0c6f143a20e66
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-13 23:16:54 -0300

    13/06 23:16

--- publico/scripts/login.js
+++ publico/scripts/login.js
@@ -1,7 +1,9 @@
 passwordInput = document.querySelector("input[name=password]");
 
-passwordInput.addEventListener("change", event =>{
-    let passwordLength = event.target.value.length;
-    console.log(event.target.value[passwordLength - 1]);
-    event.target.value[passwordLength - 1] = "*";
-});
+// passwordInput.addEventListener("change", event =>{
+//     let passwordLength = event.target.value.length;
+//     console.log(event.target.value[passwordLength - 1]);
+//     event.target.value[passwordLength - 1] = "*";
+// });
+let passwordLength = event.target.value.length;
+console.log(event.target.value[passwordLength - 1]);
