aa3f6317b0082296792bba7237b0bbf15c5d1ef4
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-14 00:47:04 -0300

    14/06 00:47

--- publico/scripts/login.js
+++ publico/scripts/login.js
@@ -3,6 +3,6 @@ const passwordInput = document.querySelector("input[name=password]");
 passwordInput.addEventListener("keydown", hidePassword);
 
 function hidePassword(event){
-    passwordInput.value += "*";
+    passwordInput.value = "*";
     //passwordInput.value -= event.key;
 };
