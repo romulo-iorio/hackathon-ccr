4fde68bfa6796059ba3fa9d619d8c053e0651cb1
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-13 22:28:53 -0300

    13/06 22:28

--- publico/scripts/login.js
+++ publico/scripts/login.js
@@ -4,7 +4,7 @@ passwordInput.addEventListener("change", hidePassword);
 
 function hidePassword(event){
     console.log("mudou!")
-    passwordText = event.target.value;
+    const passwordText = event.target.value;
     passwordText = "";
     passwordText = "*";
 }
