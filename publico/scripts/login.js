1490561301583f191a48491140316090b5aa7144
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-14 00:13:26 -0300

    14/06 00:13[

--- publico/scripts/login.js
+++ publico/scripts/login.js
@@ -5,11 +5,11 @@ let passwordPosition = password.length - 1;
 
 passwordInput.addEventListener("input", hidePassword);
 
-function hidePassword(){
+function hidePassword(event){
     password = passwordInput.value;
     passwordPosition = password.length - 1;
     console.log(password);
     console.log(passwordPosition);
     console.log(password[passwordPosition]);
-    event.target.value[passwordPosition] += "*";
+    event.target.value[passwordPosition] = "*";
 };
