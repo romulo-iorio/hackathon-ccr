84441d80ffde1ac20103d323ac192c7ca1841327
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-14 18:31:31 -0300

    18:31

--- publico/scripts/services.js
+++ publico/scripts/services.js
@@ -1,13 +1,13 @@
-const card = document.querySelector('#item1');
+const card = document.querySelector('div[name=ambev]');
+const modal = document.getElementById('modal-promocao');
 
-card.addEventListener('click', () => iniciaModal('modal-promocao'));
+card.addEventListener('click', iniciaModal);
 
-function iniciaModal(modalId) {
-    const modal = document.getElementById(modalId);
+function iniciaModal() {
     if (modal) {
         modal.classList.add('mostrar');
         modal.addEventListener('click', (event) => {
-            if(event.target.id == modalId || event.target.className == 'fechar') {
+            if(event.target.id == "modal-promocao" || event.target.className == 'fechar') {
                 modal.classList.remove('mostrar');
                 // localStorage.fecharModal = modalId;
             }
