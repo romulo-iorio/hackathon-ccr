e94ed97f91f79ad2df1b7dee4e94a46d60145f89
Rômulo Iorio <romuloiorio@hotmail.com>
2020-06-14 00:01:27 -0300

    14/06 00:01

--- publico/scripts/login.js
+++ publico/scripts/login.js
@@ -5,7 +5,7 @@ let passwordPosition = password.length - 1;
 
 passwordInput.addEventListener("input", hidePassword);
 
-function hidePassword(event){
+function hidePassword(){
     password = passwordInput.value;
     passwordPosition = password.length - 1;
     console.log(password);
